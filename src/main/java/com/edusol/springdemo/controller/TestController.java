b5baf80550118ae881b1fbb3852603dcabcd1356
Omi197 <umeshkumarmaurya.98@gmail.com>
2020-03-02 17:05:25 +0530

    added delete service

--- src/main/java/com/edusol/springdemo/controller/TestController.java
+++ src/main/java/com/edusol/springdemo/controller/TestController.java
@@ -16,6 +16,7 @@ public class TestController {
     @Autowired
     private TestService testService;
 
+
     @GetMapping("/get")
     public List<Customer> helloWorld() {
         log.info("Inside Controller");
@@ -25,4 +26,14 @@ public class TestController {
     public void addcustomer(@RequestBody Customer customer) {
         testService.addCustomerService(customer);
     }
+
+
+    @DeleteMapping("delete/{id}")
+    public String deleteCustomer(@PathVariable String id)
+    {
+        testService.deleteCustomer(id);
+        return " DELETE "+id;
+
+    }
+
 }
--- src/main/java/com/edusol/springdemo/service/TestService.java
+++ src/main/java/com/edusol/springdemo/service/TestService.java
@@ -6,24 +6,35 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Slf4j
-public class TestService
-{   @Autowired
+public class TestService {
+    @Autowired
     private TestRepository testRepository;
-        public List<Customer> getHelloWorld()
-        {
-            log.info("inside Service");
-            log.info("Gettting Customer data");
-            return testRepository.getCustomers();
+    List<Customer> customerList;
+
+    public List<Customer> getHelloWorld() {
+        log.info("inside Service");
+        log.info("Gettting Customer data");
+        return testRepository.getCustomers();
 
 
+    }
+
+    public void addCustomerService(Customer customer) {
+
+        testRepository.addCustomer(customer);
+    }
+
+    public void deleteCustomer(String id) {
+
+        customerList = testRepository.getCustomers();
+        for (Customer c : customerList) {
+            if (c.getId().equals(id)) {
+                customerList.remove(c);
+            }
         }
-        public void addCustomerService(Customer customer){
-
-            testRepository.addCustomer(customer);
-        }
+    }
 }
